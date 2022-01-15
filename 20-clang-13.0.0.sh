5d5f11e5876d658682cbb9e7747492b64623d831
btmcg <btmcg@eolas>
2022-01-14 19:44:36 -0500

    prefix fix and rpath

--- 20-clang-13.0.0.sh
+++ 20-clang-13.0.0.sh
@@ -3,17 +3,18 @@
 set -o nounset
 set -o errexit
 
-prefix=/opt/custom/clang/11.2.0
+prefix=/opt/custom/clang/13.0.0
 
 if [ -d "${prefix}" ]; then
-    printf "clang 11.2.0 already installed\n"
+    printf "clang 13.0.0 already installed\n"
     exit 0
 fi
 
 git clone --recursive --branch=llvmorg-13.0.0 --depth=1 https://github.com/llvm/llvm-project.git
 pushd llvm-project
-cmake -S llvm -B build -G "Unix Makefiles"                                                              \
+cmake -S llvm -B build -G "Ninja"                                                                       \
     -DCMAKE_BUILD_TYPE=Release                                                                          \
+    -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON                                                                 \
     -DCMAKE_C_COMPILER=/opt/custom/gcc/11.2.0/bin/gcc                                                   \
     -DCMAKE_C_FLAGS=-I/opt/custom/gcc/11.2.0/include                                                    \
     -DCMAKE_CXX_COMPILER=/opt/custom/gcc/11.2.0/bin/g++                                                 \
